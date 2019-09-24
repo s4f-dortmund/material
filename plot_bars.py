b63e7a839bafce7f20afca0d8dcdf952d529e799
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-09-24 17:40:04 +0200

    Improve script

--- plot_bars.py
+++ plot_bars.py
@@ -6,8 +6,15 @@ import pandas as pd
 import requests
 from io import StringIO
 from argparse import ArgumentParser
+import rdbu_uniform
 
 URL = 'https://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/time_series/HadCRUT.4.6.0.0.annual_ns_avg.txt'
+FIRST = 1850
+LAST = 2018  # inclusive
+
+# Reference period for the center of the color scale
+FIRST_REFERENCE = 1971
+LAST_REFERENCE = 2000
 LIM = 0.7
 
 cmap = ListedColormap([
@@ -50,17 +57,18 @@ if __name__ == '__main__':
         header=None,
     )
 
-    year_median = df.loc[1850:2018, 'anomaly'].dropna()
-    center = year_median.loc[1971:2000].mean()
-
-    print(cmap.colors)
+    year_median = df.loc[FIRST:LAST, 'anomaly'].dropna()
+    center = year_median.loc[FIRST_REFERENCE:LAST_REFERENCE].mean()
 
     fig = plt.figure(figsize=(args.width, args.height))
 
     ax = fig.add_axes([0, 0, 1, 1])
     ax.set_axis_off()
 
-    rectangles = [Rectangle((year, 0), 1, 1) for year in year_median.loc[1850:2018].index]
+    rectangles = [
+        Rectangle((year, 0), 1, 1)
+        for year in year_median.loc[FIRST:LAST].index
+    ]
     col = PatchCollection(rectangles)
     col.set_array(year_median)
     col.set_cmap(cmap)
@@ -70,6 +78,6 @@ if __name__ == '__main__':
 
     ax.set_ylim(0, 1)
     ax.set_yticks([])
-    ax.set_xlim(1850, 2019)
+    ax.set_xlim(FIRST, LAST + 1)
 
     fig.savefig(args.outputfile, dpi=args.dpi)
